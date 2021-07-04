b46d9e90dd518be14077853cf44b0e13264d5525
Alessandro <giuffra.alessandro@gmail.com>
2021-07-03 20:14:32 -0500

    collaborations 1.0v

--- lecchain-app/src/App.js
+++ lecchain-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Navbar from './components/navbar';
 import Footer from './components/footer';
+import Collaborations from './components/collaborations';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <Navbar />
+        <Collaborations />
         <Footer />
       </React.Fragment>
     );
